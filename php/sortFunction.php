d1e911c1bc9a87976addba02e556fe907abf7e9e
vaisakhp98 <76683034+vaisakhp98@users.noreply.github.com>
2022-03-03 12:03:43 +0530

    sortFunction

---
+++ php/sortFunction.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<?php
+$a=array(20, 15, 8, 1, 14, 2);
+echo "Array Before Sorting";
+foreach($a as $i)
+{
+   echo " $i ";
+}
+echo "</br> Array after sorting";
+Arraysort($a);
+function ArraySort($a)
+{
+   $n=count($a);
+   for($i=0;$i<$n-1;$i++)
+   {
+     for($j=$i+1;$j<$n;$j++)
+     {
+       if($a[$i]>$a[$j])
+       {
+          $temp=$a[$i];
+          $a[$i]=$a[$j];
+          $a[$j]=$temp;
+       }
+     }
+   }
+foreach($a as $i)
+{
+   echo " $i ";
+}
+}
+?>
+</body>
+</html>
