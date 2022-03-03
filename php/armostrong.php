7f9b0a8729ea17834c1e8ee6d7608a2f362c147c
vaisakhp98 <76683034+vaisakhp98@users.noreply.github.com>
2022-03-03 11:48:45 +0530

    Armstrong

---
+++ php/armostrong.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<h1>Armstrong Numbers </h1>
+<form method="POST" action = "#">
+<input type="number" name="number" placeholder="NumberHere"/>
+<button name="submit" type="submit">Submit</button>
+</form>
+<?php
+if(isset($_POST['submit']))
+{
+$a = $_POST['number'];
+$b = $a;
+$sum = 0;
+$ld = 0;
+while($a>=1){
+	$ld = $a%10;
+	$e = $ld*$ld*$ld;
+	$sum = $sum + $e;
+	$a = $a/10;
+}
+if($b==$sum){
+echo $b." is Armstrong";
+}else{
+echo $b." is Not Armstrong";
+}
+}
+
+?>
+</body>
+</html>
